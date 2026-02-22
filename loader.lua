4ea761489c84541dbc84b83961aa0b56f440b925
valsuden <valsuden5@gmail.com>
2026-02-22 06:05:16 -0500

    Change background image asset ID

--- loader.lua
+++ loader.lua
@@ -74,7 +74,7 @@ StrokeMain.Parent = Main
 local BackgroundImage = Instance.new("ImageLabel")
 BackgroundImage.Size = UDim2.new(1,0,1,0)
 BackgroundImage.BackgroundTransparency = 1
-BackgroundImage.Image = "rbxassetid://96939198021601"
+BackgroundImage.Image = "rbxassetid://17470839754"
 BackgroundImage.ScaleType = Enum.ScaleType.Crop
 BackgroundImage.Parent = Main
 -- Título
